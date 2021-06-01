c6cc97af98a0bd2607a1efc13ba48a9520ac3164
MunimHalim <g91314@gmail.com>
2021-06-01 08:12:21 +0800

    basic

---
+++ basic/src/basic/New.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class New {
+	
+	public static void main(String[] args) {
+		System.out.println("basic");
+		
+		method3();
+		
+		method2();
+	}
+	
+	static void method2( ) {
+		System.out.println("basic, method2");
+	}
+	
+	static void method3( ) {
+		System.out.println("basic, method3");
+	}
+}
